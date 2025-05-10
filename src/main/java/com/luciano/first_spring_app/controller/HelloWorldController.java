ca3861cc7943fecbf69efe609e3e6a3012c35fc2
Luciano Garcia <lugarsp@yahoo.com.br>
2025-05-10 17:30:17 -0300

    .

--- src/main/java/com/luciano/first_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/luciano/first_spring_app/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.luciano.first_spring_app.controller;
 
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
+    // Fica escutando o metodo GET
+    // GET /hello-world
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World";
+    }
 }
