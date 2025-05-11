77761079882731df35b8a969098e97e1c0c303c9
Luciano Garcia <lugarsp@yahoo.com.br>
2025-05-11 01:50:30 -0300

    Primeira API Spring Boot

---
+++ src/main/java/com/luciano/first_spring_app/configuration/HelloConfiguration.java
@@ -0,0 +1,5 @@
+package com.luciano.first_spring_app.configuration;
+
+public class HelloConfiguration {
+
+}
--- src/main/java/com/luciano/first_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/luciano/first_spring_app/controller/HelloWorldController.java
@@ -1,18 +1,35 @@
 package com.luciano.first_spring_app.controller;
 
+
+import com.luciano.first_spring_app.domain.User;
+import com.luciano.first_spring_app.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.GsonBuilderUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
+    @Autowired
+    private HelloWorldService helloWorldService;
+
+    //public HelloWorldController(HelloWorldService helloWorldService){
+    //    this.helloWorldService = helloWorldService;
+    //}
+
     // Fica escutando o metodo GET
     // GET /hello-world
     @GetMapping
     public String helloWorld(){
-        return "Hello World";
+        //return "Hello Controller";
+        return helloWorldService.helloWorld("Luciano");
     }
+
+    @PostMapping("/{id}")
+    public String helloWorldPost(@PathVariable("id") String id, @RequestBody User body){
+        String name = body.getName();
+        return "Hello Controller POST = " + name + id;
+    }
+
 }
